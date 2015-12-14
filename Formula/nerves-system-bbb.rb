8475f9ccb1b41f55a02d72b0df1b405221c5bf7e
Stanley Tan <hi@stnly.com>
2015-12-14 22:51:46 +0800

    Edit URLs for project rename

--- Formula/nerves-system-bbb.rb
+++ Formula/nerves-system-bbb.rb
@@ -1,8 +1,8 @@
 class NervesSystemBbb < Formula
   desc "Nerves SDK compiled for BeagleBone Black"
-  homepage "https://github.com/nerves-project/nerves-sdk"
+  homepage "https://github.com/nerves-project/nerves-system-br"
 
-  url "https://nerves-releases.s3.amazonaws.com/nerves-sdk/nerves-system-nerves_bbb-master.tar.gz"
+  url "https://nerves-releases.s3.amazonaws.com/nerves-system-br/nerves-system-nerves_bbb-master.tar.gz"
   sha256 "be190da34d41beedcce34d6d00d5965486dc536d7b02c5b2154edcb73116f522"
   version "master"
 
--- Formula/nerves-system-rpi.rb
+++ Formula/nerves-system-rpi.rb
@@ -1,8 +1,8 @@
 class NervesSystemRpi < Formula
   desc "Nerves SDK compiled for the original Raspberry Pi"
-  homepage "https://github.com/nerves-project/nerves-sdk"
+  homepage "https://github.com/nerves-project/nerves-system-br"
 
-  url "https://nerves-releases.s3.amazonaws.com/nerves-sdk/nerves-system-nerves_rpi_elixir-master.tar.gz"
+  url "https://nerves-releases.s3.amazonaws.com/nerves-system-br/nerves-system-nerves_rpi_elixir-master.tar.gz"
   sha256 "7e1adfb4bea8712f657ac73c72728f85066697e6d3bce163aaaf5027a1a1eec7"
   version "master"
 
