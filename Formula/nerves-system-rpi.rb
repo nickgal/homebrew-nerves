93e366811cae2514356b2da83d564faf1b3478fb
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-12-14 22:51:58 -0800

    remove sha256 value on nerves-system formulae
    references and closes #4 and #3

--- Formula/nerves-system-rpi.rb
+++ Formula/nerves-system-rpi.rb
@@ -3,7 +3,7 @@ class NervesSystemRpi < Formula
   homepage "https://github.com/nerves-project/nerves-system-br"
 
   url "https://nerves-releases.s3.amazonaws.com/nerves-system-br/nerves-system-nerves_rpi_elixir-master.tar.gz"
-  sha256 "7e1adfb4bea8712f657ac73c72728f85066697e6d3bce163aaaf5027a1a1eec7"
+  sha256 ""
   version "master"
 
   keg_only "Can conflict with host system, so don't symlink"
--- Formula/nerves-system-rpi2.rb
+++ Formula/nerves-system-rpi2.rb
@@ -3,7 +3,7 @@ class NervesSystemRpi2 < Formula
   homepage "https://github.com/nerves-project/nerves-system-br"
 
   url "https://nerves-releases.s3.amazonaws.com/nerves-system-br/nerves-system-nerves_rpi2_elixir-master.tar.gz"
-  sha256 "c613206040c5c65114d8f4b91013508dc9e240513da152d8bc285992c9d4cfe4"
+  sha256 ""
   version "master"
 
   keg_only "Can conflict with host system, so don't symlink"
