056fdf2486618961c2a8485bebb170ca5008d20a
Nick Gal <nickgal@me.com>
2015-12-14 23:15:36 -0800

    Update test to fetch & reinstall cached nerves system packages.

--- test.rb
+++ test.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 require 'fileutils'
 require 'test/unit'
 extend Test::Unit::Assertions
@@ -9,6 +10,11 @@ platforms = %w(bbb rpi rpi2)
 platforms.each do |pm|
   FileUtils.rm_rf @dirname
   puts pm
+
+  # Check nerves-system packages. Force fetch to bypass homebrew cache.
+  # Revisit with tagged nerves-system releases.
+  assert system("brew fetch --force nerves-system-#{pm} && brew reinstall nerves-system-#{pm}")
+
   assert system("brew nerves new #{pm} #{@dirname}")
   assert system(%{bash -c "cd #{@dirname} && source nerves-env.sh && make"})
 end
